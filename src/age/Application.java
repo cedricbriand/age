78f03e53558c25f6ad217fa668a6055dea723391
BRIAND <cedric.briand@capgemini.com>
2016-10-28 11:27:05 +0200

    toto

--- src/age/Application.java
+++ src/age/Application.java
@@ -1,10 +1,13 @@
 package age;
 
+import com.mongodb.client.MongoDatabase;
+
 public class Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("toto");
+		MongoDatabase mongoDatabase;
 		int nb = 5;
 	}
 
