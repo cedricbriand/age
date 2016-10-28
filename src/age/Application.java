3f695cec05cc8d90f3642f392cae9a2d01ce2417
BRIAND <cedric.briand@capgemini.com>
2016-10-28 11:51:53 +0200

    nbre

--- src/age/Application.java
+++ src/age/Application.java
@@ -7,8 +7,9 @@ public class Application {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("totu");
-		MongoDatabase mongoDatabase;
-		int nb = 7;
+		MongoClient cl = new MongoClient("localhost",27017);
+		MongoDatabase mongoDatabase = cl.getDatabase("Test");
+		int nb = 9;
 	}
 
 }
